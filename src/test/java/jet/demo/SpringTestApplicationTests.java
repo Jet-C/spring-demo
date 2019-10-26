93a7444fae7e753be8fdcc2a008d18d2b154faa5
Jetzy <jecabral1@outlook.com>
2019-10-26 00:20:55 -0500

    Initial commit and build

---
+++ src/test/java/jet/demo/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jet.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
